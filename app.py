6e68785f8e43913d1f24f4848d5e2addc8858589
jdtluis <83558495+jdtluis@users.noreply.github.com>
2023-03-30 15:10:00 -0300

    script no ejecutable

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def index(chartID = 'chart_ID', chart_height = 400): #
 		container = {}
 
 		for s in letras:
-			url = f"http://127.0.0.1:5000/letras?tipo={s}&date={date}"
+			url = f"https://api-letras.onrender.com/letras?tipo={s}&date={date}"
 			r = requests.get(url)
 			data = pd.DataFrame(r.json()) #getdata(s, date)
 			print(data)
@@ -51,4 +51,4 @@ def index(chartID = 'chart_ID', chart_height = 400): #
 
 
 #if __name__ == "__main__":
-#	app.run(debug = True, passthrough_errors=True, port=8080) #, host='0.0.0.0', port=8080
+	#app.run(debug = True, passthrough_errors=True, port=8080) #, host='0.0.0.0', port=8080
